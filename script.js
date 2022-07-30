00758bb44eb78c883bdec5c1ab2c762ef7864448
Rodrigo Bonzerr S. Lopez <44669373+rodprogramdev@users.noreply.github.com>
2022-07-30 20:16:38 +0800

    timer and random quote working

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random'
+const RANDOM_QUOTE_API_URL =   'http://api.quotable.io/random' //https://www.youtube.com/watch?v=fujfX2fIoyo
 const textDisplayElement = document.getElementById('textDisplay')
 const textInputElement = document.getElementById('textInput')
 const timerElement = document.getElementById('timer')
+
+//https://www.freecodecamp.org/news/creating-a-bare-bones-quote-generator-with-javascript-and-html-for-absolute-beginners-5264e1725f08/
+
+
+// function newQuote(){
+//     var qoutes = [
+//         'Learning is a matter of finding your purpose, find your purpose and you will be the wisest man who ever lived',
+//         'Be careful with the type of leader you will follow, a leader who tells you your not ready instead of telling you try it out and if you make a mistake, treat it as a learning experience.',
+//         'Fear is my friend, it limits me in doing certain things but it pushes me to extrems I can never imagine'
+//     ]
+//     var randomNumber = Math.floor(Math.random() * (qoutes.length));
+//     return newQuote
+
+// }
 
 
 textInputElement.addEventListener('input', () => {
@@ -36,13 +50,14 @@ function getRandomQuote() {
     .then(response => response.json())
     .then(data => data.content)
 }
-/*
+
 async function getNextQuote() {
     const quote = await getRandomQuote()
     console.log(quote)
 getNextQuote()
 
-}*/
+}
+
 async function renderNewQuote() {
     const quote = await getRandomQuote()
     console.log(quote)
